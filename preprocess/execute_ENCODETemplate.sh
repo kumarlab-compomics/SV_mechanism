f6822f0ca20d2b4dda1a773bbe08ff152277d0b8
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-01 14:02:51 -0500

    Update execute_ENCODETemplate.sh

--- preprocess/execute_ENCODETemplate.sh
+++ preprocess/execute_ENCODETemplate.sh
@@ -9,30 +9,34 @@
 #SBATCH --error=/home/nboev/projects/def-sushant/nboev/preprocess/PROJECT/LOCATION/ENCODE/log/ENCODECHROM_%j.e
 #SBATCH --array=0-0
 
+# Loading in virtual environment and modules
 module --force purge ; module load StdEnv/2020
 source $HOME/virtenv/bin/activate
 module load scipy-stack
 
-# looping through files from the split
+# For this chromosome, we have a job for each split
 names=($(cat /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/CHROM/splitnames.txt))
 files=${names[${SLURM_ARRAY_TASK_ID}]}
-echo "${files}"
 filename=$(basename "$files")
 filenamenoext=$(basename "${filename%.*}")
 
-echo "${filename}"
-echo "${filenamenoext}"
+# The input (comes from execute_Master1.sh)
+# 1: Project name and location
+# 2: The type of SVs to annotate (ie. sv or svSim). This is a subdirectory that holds the vcf/csv file
+# 3: Cell line name to use for epigenomic annotation (must lead to the location of the ENCODE files)
 
-python adding_epiFeaturesSV_240517.py \
+# We now push this python script which will annotate the SVs' epigenetic features
+python adding_epiFeaturesSV.py \
 ${files} \
 $1 $2 \
 ${filenamenoext} \
 CHROM \
 /home/nboev/projects/def-sushant/nboev/data/ENCODE/$3/ \
 $3 \
->> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/adding_epiFeaturesSV_240517.py.txt
+>> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/adding_epiFeaturesSV.py.txt
 
-python adding_epiFeaturesflanks_240517.py \
+# We now push this python script which will annotate the SVs flanking (2000bp) epigenetic features
+python adding_epiFeaturesflanks.py \
 ${files} \
 $1 $2 \
 ${filenamenoext} \
@@ -40,4 +44,4 @@ CHROM \
 /home/nboev/projects/def-sushant/nboev/data/ENCODE/$3/ \
 2000 \
 $3 \
->> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/adding_epiFeaturesflanks_240517.py.txt
+>> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/adding_epiFeaturesflanks.py.txt
