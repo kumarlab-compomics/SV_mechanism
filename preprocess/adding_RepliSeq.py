0ca5c7c888059b11abb57a330b18755f8d99f68e
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-01 14:41:37 -0500

    Update and rename adding_RepliSeq_240517.py to adding_RepliSeq.py

--- preprocess/adding_RepliSeq.py
+++ preprocess/adding_RepliSeq.py
@@ -8,7 +8,8 @@ from pytz import timezone
 
 print('START TIME:', datetime.datetime.now(timezone('EST')))
 
-# loading in the Repliseq data
+# In this script, we take in a vcf/csv batch file and annotate the SV's position with it's replication timing (as measured by S50 from a RepliSeq experiment)
+
 repli = pd.read_csv(str(argv[1]), sep=',')
 svs = pd.read_csv(str(argv[2]), comment='#', sep='\t')
 project = str(argv[3])
@@ -22,23 +23,18 @@ svs = svs[svs['CHROM'] == chr]
 ogrow = len(svs)
 print('og rows:', ogrow)
 
+# We get the start and stop positions of the Repliseq and use these intervals to annotate the SV position
 idx = pd.IntervalIndex.from_arrays(repli['start'], repli['stop'], closed='left')
 repli.index=idx
-
 svs['S50']=repli.loc[svs.POS,'S50'].values
 
 newrow = len(svs)
 print('new rows:', newrow)
 
-# checking if the rows match (they should)
+# Checking if we lose any of the rows in this process. If the new and old number of rows match, we save the annotated file
 if ogrow == newrow:
-	print('good merge')
 	svs.to_csv('/home/nboev/projects/def-sushant/nboev/preprocess/'+project+'/'+loc+'/RepliSeq/'+chr+'/' +filename+ '.withRepliSeq.csv', sep='\t', index=False)
 else:
 	print('poor merged')
 
-
 print('END TIME:', datetime.datetime.now(timezone('EST')))
-
-
-
