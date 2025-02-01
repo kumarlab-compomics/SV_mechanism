a0721963bcf04b0039842b5faf9bc1f289dde313
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-01 15:08:51 -0500

    Update execute_seqFeaturesSVTemplate.sh

--- preprocess/execute_seqFeaturesSVTemplate.sh
+++ preprocess/execute_seqFeaturesSVTemplate.sh
@@ -9,25 +9,25 @@
 #SBATCH --error=/home/nboev/projects/def-sushant/nboev/preprocess/PROJECT/LOCATION/seqFeaturesSV/log/seqFeaturesCHROM_%j.e
 #SBATCH --array=0-0
 
+# Loading in virtual environment and modules
 module --force purge ; module load StdEnv/2020
 source $HOME/virtenv/bin/activate
 module load scipy-stack
 
-# looping through files from the split
+# For this chromosome, we have a job for each split
 names=($(cat /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/CHROM/splitnames.txt))
 files=${names[${SLURM_ARRAY_TASK_ID}]}
-echo "${files}"
 filename=$(basename "$files")
 filenamenoext=$(basename "${filename%.*}")
 
-echo "${filename}"
-echo "${filenamenoext}"
+# The input (comes from execute_Master1.sh)
+# 1: Project name and location
+# 2: The type of SVs to annotate (ie. sv or svSim). This is a subdirectory that holds the vcf/csv file
 
-# I think we need to add something in the python file so they don't overwrite!
+# We now push this python script which will annotate the SV's sequence features
 python adding_seqFeaturesSV_240118.py \
 ${files} \
 $1 $2 \
 ${filenamenoext} \
 CHROM \
 >> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/adding_seqFeaturesSV_240118.py.txt
-
