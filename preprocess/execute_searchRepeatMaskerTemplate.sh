116420db3d29d8e5e7acba7ba4d3e611915a3bb1
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-01 14:54:51 -0500

    Update execute_searchRepeatMaskerTemplate.sh

--- preprocess/execute_searchRepeatMaskerTemplate.sh
+++ preprocess/execute_searchRepeatMaskerTemplate.sh
@@ -9,24 +9,28 @@
 #SBATCH --error=/home/nboev/projects/def-sushant/nboev/preprocess/PROJECT/LOCATION/searchRepeatMasker/log/searchRepeatMaskerCHROM_%j.e
 #SBATCH --array=0-0
 
+# Loading in virtual environment and modules
+# In this case, we need repeatmasker
 module --force purge ; module load StdEnv/2020 gcc/9.3.0
 source $HOME/virtenv/bin/activate
 module load scipy-stack
 module load repeatmasker
 
-# looping through files from the split
+# For this chromosome, we have a job for each split
 names=($(cat /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/CHROM/splitnames.txt))
 files=${names[${SLURM_ARRAY_TASK_ID}]}
-echo "${files}"
 filename=$(basename "$files")
 filenamenoext=$(basename "${filename%.*}")
 
-echo "${filename}"
-echo "${filenamenoext}"
+# The input (comes from execute_Master1.sh)
+# 1: Project name and location
+# 2: The type of SVs to annotate (ie. sv or svSim). This is a subdirectory that holds the vcf/csv file
 
+# Make a directory to hold the RepeatMasker results, thereby to not overwrite the directories RepeatMasker will generate
 mkdir /home/nboev/projects/def-sushant/nboev/preprocess/PROJECT/LOCATION/searchRepeatMasker/CHROM/${filenamenoext}
 
-python adding_searchRepeatMasker_240521.py \
+# We push a python script which sends the DEL or INS sequence through RepeatMasker
+python adding_searchRepeatMasker.py \
 ${files} \
 $1 $2 \
 ${filenamenoext} \
@@ -37,14 +41,14 @@ rm /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/searchRepeatMasker/CH
 rm /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/searchRepeatMasker/CHROM/${filenamenoext}/${filenamenoext}.fa.masked
 rm /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/searchRepeatMasker/CHROM/${filenamenoext}/${filenamenoext}.fa.cat
 
-python adding_searchRepeatMaskermerges_240521.py \
+# We then push a python script which will append the RepeatMasker output to the vcf/csv file
+python adding_searchRepeatMaskermerges.py \
 ${files} \
 $1 $2 \
 ${filenamenoext} \
 CHROM \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/searchRepeatMasker/CHROM/${filenamenoext}/${filenamenoext}.fa.out \
 >> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/adding_searchRepeatMaskermerges_240521.py.txt
-
 
 rm /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/searchRepeatMasker/CHROM/${filenamenoext}/${filenamenoext}.fa
 rm /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/searchRepeatMasker/CHROM/${filenamenoext}/${filenamenoext}.fa.out
