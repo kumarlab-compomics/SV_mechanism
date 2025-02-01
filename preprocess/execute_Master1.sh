9f8939a846b5d9b63eafa2157594d04129fab104
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-01 13:32:54 -0500

    Update execute_Master1.sh

--- preprocess/execute_Master1.sh
+++ preprocess/execute_Master1.sh
@@ -10,39 +10,35 @@
 #SBATCH --qos=privilege
 #SBATCH --array=0-24
 
-# in this script, we need to push each chromosome through job arrays
+# Loading in virtual environment and modules
 module --force purge ; module load StdEnv/2020
 source $HOME/virtenv/bin/activate
 module load gcc/9.3.0 r/4.0.2
 module load scipy-stack
 
-# for now, we'll use the chromo list in the simulation directory
+# To run 24 jobs in array, based on this list of chromosomes
 names=($(cat ./chromos.txt))
-
 files=${names[${SLURM_ARRAY_TASK_ID}]}
 echo "${files}"
 
+# The input: 
+# 1: Project name and location
+# 2: The type of SVs to annotate (ie. sv or svSim). This is a subdirectory that holds the vcf/csv file
+# 3: The name of the file to annotate 
+# 4: Cell line name to use for epigenomic annotation (must lead to the location of the ENCODE files)
 
-# I need to split up the file by chromosome and make splits we can actually annotate
-# In this case, maybe we can make row splits of 10K per chromosome
-
-# we might need to use an argument for the executes, for example which location we'd actually be placing these files
-# ie. HGSVC + svSim vs sv etc..
+# Creating a directory which will hold these chromosomes files and filtering the original vcf for this chromosome
 mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}
-
-# need to make the directory below include arguments!
-# want to keep the header of the file
 awk '$1 == "'${files}'" { print }' /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/$3 \
 > /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/${files}.full.csv
 
-# making splits of 10K, might need to increase later
+# We take the filtered file and make 10K splits for this chromosomes
 split -l 10000 -d --additional-suffix=.csv \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/${files}.full.csv \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/${files}.full.csv.split
 
-# getting header
+# Get the header so we can append to the batched files
 header=$(head -n 1 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/$3)
-
 for f in /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/${files}.full.csv.split*;
 	do
 	echo "$header" > "${f}.csv"
@@ -53,11 +49,13 @@ for f in /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/
 splits=$(ls -1 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/${files}.full.csv.split* | wc -l)
 echo "${splits}"
 
+# Getting a list of the number of splits we created for this chromosome
 ls /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/${files}.full.csv.split* \
 > /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/splitnames.txt
-
 rm /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/${files}.full.csv
 
+# For the following annotations (chromoBand seqFeaturesSV flankSeq SVcoords RepliSeq searchRepeatMasker ENCODE):
+# We make a copy of the "template" script and use the number of splits to push job arrays for this annotation time
 for script in chromoBand seqFeaturesSV flankSeq SVcoords RepliSeq searchRepeatMasker ;
 	do
 	mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/${script}
@@ -70,7 +68,7 @@ for script in chromoBand seqFeaturesSV flankSeq SVcoords RepliSeq searchRepeatMa
 	sbatch /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/${script}/execute_${script}_${files}.sh $1 $2
 	done
 
-# we need this for the ENCODE files, we need to add the specificatio of the cell line
+# In this case, we specify the cell type to use for epigenomic annotation
 mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/ENCODE
 mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/ENCODE/$4
 cp execute_ENCODETemplate.sh /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/ENCODE/execute_ENCODE_${files}.sh
@@ -81,7 +79,7 @@ sed -i "s/LOCATION/$2/g" /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2
 mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/ENCODE/$4/${files}
 sbatch /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/ENCODE/execute_ENCODE_${files}.sh $1 $2 $4
 
-# we need to add this for the flank annotations:
+# We require the following directories to hold the pre and post processed files
 for script in RLoopForming RepeatMasker nonBDNA Blast DNAshape ;
 	do
 	mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/${script}
@@ -90,7 +88,6 @@ for script in RLoopForming RepeatMasker nonBDNA Blast DNAshape ;
 	mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/${script}/${files}/postmerge
 	done
 
-# we need this for flank seq annotations:
 for script in seqFeatures ;
 	do
 	mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/${script}
@@ -98,13 +95,7 @@ for script in seqFeatures ;
 	done
 
 
-
-# how to generate: variants_freeze4_sv_insdel_alt.vcf.SVlength.Simulations.csv within SVlen: See /home/nboev/projects/def-sushant/nboev/data/SimulatedSVs/HGSVC2_v2_integrated_callset/execute_PostProcessing_240502.sh
-
-
-# we need to include arguments to actually use: Running = 2024/06/03
-
-# Doing HGSVC3 heere instead of Narval = 2024/08/
+# example:
 # sbatch execute_Master1_240502.sh allHGSVC3_20240415_Freeze4_GRCh38 sv freeze4_GRCh38_sv_insdel_alt.vcf.SVlength.csv H1
 # sbatch execute_Master1_240502.sh allHGSVC3_20240415_Freeze4_GRCh38 svSim variants_freeze4_sv_insdel_alt.vcf.SVlength.Simulations.csv H1
 
