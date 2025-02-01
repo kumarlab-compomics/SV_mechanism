af8586ed5814e7aaeb6d482f3c3ccd246f060eee
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-01 14:29:12 -0500

    Update execute_RepliSeqTemplate.sh

--- preprocess/execute_RepliSeqTemplate.sh
+++ preprocess/execute_RepliSeqTemplate.sh
@@ -9,27 +9,29 @@
 #SBATCH --error=/home/nboev/projects/def-sushant/nboev/preprocess/PROJECT/LOCATION/RepliSeq/log/RepliSeqCHROM_%j.e
 #SBATCH --array=0-0
 
+# Loading in virtual environment and modules
 module --force purge ; module load StdEnv/2020
 source $HOME/virtenv/bin/activate
 module load scipy-stack
 
-# looping through files from the split
+# For this chromosome, we have a job for each split
 names=($(cat /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/CHROM/splitnames.txt))
 files=${names[${SLURM_ARRAY_TASK_ID}]}
-echo "${files}"
 filename=$(basename "$files")
 filenamenoext=$(basename "${filename%.*}")
 
-echo "${filename}"
-echo "${filenamenoext}"
+# The input (comes from execute_Master1.sh)
+# 1: Project name and location
+# 2: The type of SVs to annotate (ie. sv or svSim). This is a subdirectory that holds the vcf/csv file
 
-
-# I think we need to add something in the python file so they don't overwrite!
-python adding_RepliSeq_240517.py \
+# We push this python script to annotate the replication timing of the SV's position
+# Note: This requires a file which is generated using RepliSeq data then processed with RepliSeq 
+  # See: https://github.com/CL-CHEN-Lab/RepliSeq 
+# The processed file (tempS50.csv) is held in the data directory
+python adding_RepliSeq.py \
 /home/nboev/projects/def-sushant/nboev/data/RepliSeq/H1/Multi-stage/tempS50.csv \
 ${files} \
 $1 $2 \
 ${filenamenoext} \
 CHROM \
 >> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/adding_RepliSeq_240517.py.txt
-
