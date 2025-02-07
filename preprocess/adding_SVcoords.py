46c292f0c54251abd07522311a4f5f270a288a7c
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-07 14:21:57 -0500

    Update adding_SVcoords.py

--- preprocess/adding_SVcoords.py
+++ preprocess/adding_SVcoords.py
@@ -27,80 +27,54 @@ The input:
 '''
 
 def pulling(df, flank) :
-	# Create an appropriate name given the length of the flank to use
-	precoord = 'pre_'+str(flank)+'_coord'
-	postcoord = 'post_'+str(flank)+'_coord'
 
 	# For each row, calculate the pre and post coordinates. Recall, for deletions, you must consider the length when "starting" the flanking position
 	for idx, row in df.iterrows():
+		df.loc[idx, 'unique_id'] = row['CHROM']+ '_' +str(row['POS'])+ '_' +str(row['SVlen'])+ '_' +row['SV_Type']
+		
 		if (row.SV_Type == 'deletion')|(row.SV_Type == 'DEL'):
-			df.loc[idx, precoord] = int(row.POS - flank)
-			df.loc[idx, postcoord] = int(row.POS + row.SVlen + flank)
+			df.loc[idx, 'preflank_start'] = row.POS - flank
+			df.loc[idx, 'preflank_end'] = row.POS
+			df.loc[idx, 'postflank_start'] = row.POS + row.SVlen
+			df.loc[idx, 'postflank_end'] = row.POS + row.SVlen + flank
 
 		else :
-			df.loc[idx, precoord] = int(row.POS - flank)
-			df.loc[idx, postcoord] = int(row.POS + flank)
+			df.loc[idx, 'preflank_start'] = row.POS - flank
+			df.loc[idx, 'preflank_end'] = row.POS
+			df.loc[idx, 'postflank_start'] = row.POS
+			df.loc[idx, 'postflank_end'] = row.POS + flank
 
 	return df
 
 # Calling the pulling function
 svs = pulling(svs, int(size))
 
-# just use the function above!!!???
-
-# we're going to save a version I can use with bedtools
-for idx, row in svs.iterrows():
-	svs.loc[idx, 'unique_id'] = row['CHROM']+ '_' +str(row['POS'])+ '_' +str(row['SVlen'])+ '_' +row['SV_Type']
-
-	if (row.SV_Type == 'deletion') | (row.SV_Type == 'DEL'):
-		svs.loc[idx, 'preflank_start'] = row['pre_'+size+'_coord']
-		svs.loc[idx, 'preflank_end'] = row.POS
-		svs.loc[idx, 'postflank_start'] = row.POS + row.SVlen
-		svs.loc[idx, 'postflank_end'] = row['post_'+size+'_coord']
-	else:
-		svs.loc[idx, 'preflank_start'] = row['pre_'+size+'_coord']
-		svs.loc[idx, 'preflank_end'] = row.POS
-		svs.loc[idx, 'postflank_start'] = row.POS
-		svs.loc[idx, 'postflank_end'] = row['post_'+size+'_coord']
-
-
-# we need this values to be integers
+# Making these columns integers
 svs['preflank_start'] = svs['preflank_start'].apply(np.int64)
 svs['preflank_end'] = svs['preflank_end'].apply(np.int64)
 svs['postflank_start'] = svs['postflank_start'].apply(np.int64)
 svs['postflank_end'] = svs['postflank_end'].apply(np.int64)
 
-
+# Since we've added these columns wide-wise, we need to do some re-formatting to convert it to long-wise
 short = svs [['CHROM', 'preflank_start', 'preflank_end', 'postflank_start', 'postflank_end', 'unique_id']]
-print(short.head())
-
 starts = short[['CHROM','preflank_start', 'postflank_start', 'unique_id']]
 ends = short[['CHROM','preflank_end', 'postflank_end', 'unique_id']]
-
 starts = pd.melt(starts, id_vars=["CHROM", "unique_id"], value_name="Start")
 ends = pd.melt(ends, id_vars=["CHROM", "unique_id"], value_name="End")
 merged = pd.merge(starts, ends, on=['CHROM', 'unique_id'])
 
-
+# Spot-checking that the difference between the start/stop matches the requested flanking length
 for idx, row in merged.iterrows():
 	merged.loc[idx, 'difference'] = int(row.End - row.Start)
 
 merged = merged[merged.difference == int(size)]
 merged = merged[['CHROM', 'Start', 'End', 'unique_id']]
 
-# getting rid of negatives
-#for idc, row in merged.iterrows():
-#	if int(row.Start) < 0:
-#		merged.loc[idx, 'Start'] = 0
-#	if int(row.End) < 0:
-#		merged.loc[idx, 'End'] = 0
-
 merged.Start = np.where(merged.Start < 0, 0, merged.Start)
 merged.End = np.where(merged.End < 0, 0, merged.End)
 
-# Saving the file itself --> this is the only one we'll save
+# Saving the bed file
 merged.to_csv('/home/nboev/projects/def-sushant/nboev/preprocess/'+project+'/'+loc+'/SVcoords/' +chr+'/'+ filename+'.SVcoords' +size+ 'flank_bedtools.bed', sep='\t', index=False, header=False)
-
 
 print('end of script')
 print('END TIME:', datetime.datetime.now(timezone('EST')))
