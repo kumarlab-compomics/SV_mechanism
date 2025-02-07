64cd60031735bed5d12d5d937e2bfcb19f3be910
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-07 14:58:06 -0500

    Update execute_flankSeqTemplate.sh

--- preprocess/execute_flankSeqTemplate.sh
+++ preprocess/execute_flankSeqTemplate.sh
@@ -29,7 +29,7 @@ filenamenoext=$(basename "${filename%.*}")
 
 # We push this python script which pulls the 2000bp flanking sequences (up and downstream) using faidx
 # Note : This script requires an accessible copy of the reference genome
-python adding_flankSeq_240517.py \
+python adding_flankSeq.py \
 ${files} \
 $1 $2 \
 ${filenamenoext} \
@@ -39,7 +39,7 @@ CHROM \
 
 # We push this python script which annotates the SV flanks with its GC content, complexity, flexibility and complexity
 # Similar to: adding_seqFeaturesSV.py
-python adding_seqFeatures_240118.py \
+python adding_seqFeatures.py \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/flankSeq/CHROM/${filenamenoext}.with2000.flankseq_samtools.csv \
 $1 $2 \
 ${filenamenoext} \
@@ -50,7 +50,7 @@ CHROM \
 # We push this script which takes the 2000bp flanking sequences to :
 # 1. Perform various blast alignments to describe local homology
 # 2. Employ DNAShapeR to determine local and SV DNA shape features
-python adding_BlastDNAShape_240522.py \
+python adding_BlastDNAShape.py \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/flankSeq/CHROM/${filenamenoext}.with2000.flankseq_samtools.csv \
 $1 $2 \
 ${filenamenoext} \
@@ -59,7 +59,7 @@ CHROM \
 >> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/adding_BlastDNAShape_240522.py.txt
 
 # We pusht his script to merge the homology results from Blast to the original vcf/csv file
-python adding_Blastmerges_240522.py \
+python adding_Blastmerges.py \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/flankSeq/CHROM/${filenamenoext}.with2000.flankseq_samtools.csv \
 $1 $2 \
 ${filenamenoext} \
