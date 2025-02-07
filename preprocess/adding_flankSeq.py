15af9e158f6b383ac92e1712266eaad7a3b256b8
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-07 15:06:32 -0500

    Update and rename adding_flankSeq_240517.py to adding_flankSeq.py

--- preprocess/adding_flankSeq.py
+++ preprocess/adding_flankSeq.py
@@ -8,12 +8,12 @@ import datetime
 from pytz import timezone
 from Bio import SeqIO
 
-
 print('\n ********************************')
 print('START TIME:', datetime.datetime.now(timezone('EST')))
 
-# loading in SVs
-print(argv[1])
+# In this script, we will determine the coordinates of the flanks, retrieve the matching sequences from the reference genome, and append these sequences to the original vcf/csv
+
+# Data inputs sent in from the execution files
 svs = pd.read_csv(str(argv[1]), comment='#', sep='\t')
 project = str(argv[2])
 loc = str(argv[3])
@@ -21,26 +21,33 @@ filename = str(argv[4])
 chr = str(argv[5])
 flank = str(argv[6])
 
-ogrow = len(svs)
-print('og rows:', ogrow)
-
+# Designating the appropriate column names based on the requested flank length
 pre_str = "pre_flank_seq_"+str(flank)
 post_str = "post_flank_seq_"+str(flank)
 
 df = svs[svs.CHROM == chr]
 
+# The following loops through all the rows in the vcf/csv file to pull the reference sequence
+# The insertion/deletion procedure are the same, whereby the difference lies in the downstream flank coordinates, since deletions require the consideration of the SV length
+
 for idx, row in df[df.SV_logic == True].iterrows():
 	if ((row.SV_Type == 'deletion') | (row.SV_Type == 'DEL') ) :
-		prestart = int(row.POS) - int(flank)
 
+		# We identify the pre-flanking position using the requested flank length
+		prestart = int(row.POS) - int(flank)
+		# We interact with the shell system to call the samtools faidx function.
+		# We provide the pre flanking coordinates and the HG38 reference genome and save the sequence in a fasta file
 		os.system('/bin/bash -c "samtools faidx /home/nboev/projects/def-sushant/nboev/data/Genome/hg38.fa "' +chr+ '":"' +str(prestart)+ '"-"' +str(row.POS)+ '" -o /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/flankSeq/"'+chr+'"/"'+filename+'"testing.fasta"')
+		# We read this fasta file using Bio's SeqIO function
 		record = SeqIO.read("/home/nboev/projects/def-sushant/nboev/preprocess/"+project+"/"+loc+"/flankSeq/"+chr+'/'+filename+"testing.fasta", "fasta")
+		# We save this sequence in the pre_str column in a standardized format (upper case)
 		df.loc[idx, pre_str] = str(record.seq).upper()
+		# We delete the fasta file to mitigate space + overwriting issues
 		os.system('/bin/bash -c "rm /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/flankSeq/"'+chr+'"/"'+filename+'"testing.fasta"')
 
+		# We repeat the above steps using the post flanking coordinates
 		poststart = int(row.POS) + int(row.SVlen)
 		postend = int(row.POS) + int(row.SVlen) + int(flank)
-
 		os.system('/bin/bash -c "samtools faidx /home/nboev/projects/def-sushant/nboev/data/Genome/hg38.fa "' +chr+ '":"' +str(poststart)+ '"-"' +str(postend)+ '" -o /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/flankSeq/"'+chr+'"/"'+filename+'"testing.fasta"')
 		record = SeqIO.read("/home/nboev/projects/def-sushant/nboev/preprocess/"+project+"/"+loc+"/flankSeq/"+chr+'/'+filename+"testing.fasta", "fasta")
 		df.loc[idx, post_str] = str(record.seq).upper()
@@ -49,13 +56,10 @@ for idx, row in df[df.SV_logic == True].iterrows():
 
 	elif ((row.SV_Type == 'insertion') | (row.SV_Type == 'INS') ):
 		prestart = int(row.POS) - int(flank)
-
 		os.system('/bin/bash -c "samtools faidx /home/nboev/projects/def-sushant/nboev/data/Genome/hg38.fa "' +chr+ '":"' +str(prestart)+ '"-"' +str(row.POS)+ '" -o /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/flankSeq/"'+chr+'"/"'+filename+'"testing.fasta"')
 		record = SeqIO.read("/home/nboev/projects/def-sushant/nboev/preprocess/"+project+"/"+loc+"/flankSeq/"+chr+'/'+filename+"testing.fasta", "fasta")
-
 		df.loc[idx, pre_str] = str(record.seq).upper()
 		os.system('/bin/bash -c "rm /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/flankSeq/"'+chr+'"/"'+filename+'"testing.fasta"')
-
 		postend = int(row.POS) + int(flank)
 		os.system('/bin/bash -c "samtools faidx /home/nboev/projects/def-sushant/nboev/data/Genome/hg38.fa "' +chr+ '":"' +str(row.POS)+ '"-"' +str(postend)+ '" -o /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/flankSeq/"'+chr+'"/"'+filename+'"testing.fasta"')
 		record = SeqIO.read("/home/nboev/projects/def-sushant/nboev/preprocess/"+project+"/"+loc+"/flankSeq/"+chr+'/'+filename+"testing.fasta", "fasta")
@@ -63,13 +67,7 @@ for idx, row in df[df.SV_logic == True].iterrows():
 		os.system('/bin/bash -c "rm /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/flankSeq/"'+chr+'"/"'+filename+'"testing.fasta"')
 
 
-
-# calling this function
-print(df.head())
-newrow = len(df)
-print('new rows:', newrow)
-
-# saving the csv
+# Saving the annotated dataframe
 df.to_csv('/home/nboev/projects/def-sushant/nboev/preprocess/'+project+'/'+loc+'/flankSeq/'+chr+'/' +filename+ '.with' +flank+'.flankseq_samtools.csv', sep='\t', index=False)
 
 print('END TIME:', datetime.datetime.now(timezone('EST')))
