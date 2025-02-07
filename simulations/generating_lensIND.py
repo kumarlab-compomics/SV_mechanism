4c11619646178f98e228ca900ebb99656c7f6ac8
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-07 16:24:27 -0500

    Update and rename generating_lensIND_240424.py to generating_lensIND.py

--- simulations/generating_lensIND.py
+++ simulations/generating_lensIND.py
@@ -11,24 +11,23 @@ import numpy as np
 print('\n ********************************')
 print('START TIME:', datetime.datetime.now(timezone('EST')))
 
+# In this script, we take in a vcf/csv, and create matching parameter files, as per SURVIVOR requirements for SV simulation generation
+
 svs = pd.read_csv(str(argv[1]), comment='#', sep='\t')
-print(len(svs))
 chromo = str(argv[2])
-print('chr:', chromo)
 project = str(argv[4])
 loc = str(argv[5])
-
-print(list(svs.SV_Type.unique()))
 svs = svs[(svs.CHROM == chromo)]
-print(len(svs))
-print(list(svs.SV_Type.unique()))
-print(svs.head())
-print(svs.columns)
 
-# this time, we need to make a parameter file for each SV
+'''
+The following loops through all the rows in the vcf/csv file to generate custome configuration files (parameter_file)
+	Note: parameter_file was downloaded from: https://github.com/fritzsedlazeck/SURVIVOR/wiki
+ 1. We generate standardized IDs for each SV
+ 2. We generate start/stop SV lengths, as per the config file.
+ 3. We use a new location for the config file, to hold the correct INDEL_maximum_length and INDEL_maximum_length lengths, as per the SV. This is done by interacting with the shell
+'''
+
 for idx, row in svs.iterrows():
-	print(row.CHROM, row.POS+1, row.SVlen, row.SV_Type)
-
 	if (row.SV_Type == 'deletion') |(row.SV_Type == 'DEL'):
 		ID = row.CHROM +'-'+ str(row.POS+1) +'-'+ 'DEL' +'-'+ str(int(float(row.SVlen)))
 
@@ -41,9 +40,6 @@ for idx, row in svs.iterrows():
 		startlens = str(int(float(row.SVlen))+1)
 		stoplens = str(int(float(row.SVlen))+2)
 
-#       output_file = f'/home/nboev/projects/def-sushant/nboev/data/SimulatedSVs/{project}/{loc}/parameterIND/{chromo}/{ID}.param.txt'
 	output_file = f'/home/nboev/scratch/data/SimulatedSVs/{project}/{loc}/parameterIND/{chromo}/{ID}.param.txt'
-
-# need to make this correct!!
 	first = subprocess.run(['sed', f's/INDEL_minimum_length: 50/INDEL_minimum_length: {startlens}/', str(argv[3])], stdout=subprocess.PIPE)
 	subprocess.run(['sed', f's/INDEL_maximum_length: 51/INDEL_maximum_length: {stoplens}/'], input=first.stdout, stdout=open(output_file, 'w'))
