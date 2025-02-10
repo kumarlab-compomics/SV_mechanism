c20fd4bef64e56e440c987f9b3560b3b0e404bdd
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-10 14:24:39 -0500

    Update and rename execute_Master1Merge_240530.sh to execute_Master1Merge.sh

--- preprocess/execute_Master1Merge.sh
+++ preprocess/execute_Master1Merge.sh
@@ -8,40 +8,39 @@
 #SBATCH --output=MasterMerge_%j.o
 #SBATCH --error=MasterMerge_%j.e
 #SBATCH --qos=privilege
-#SBATCH --array=0-25
+#SBATCH --array=0-24
 
-# remember to change the array number back
-
-# in this script, we need to push each chromosome through job arrays
+# Loading in virtual environment and modules
 module --force purge ; module load StdEnv/2020
 source $HOME/virtenv/bin/activate
 module load gcc/9.3.0 r/4.0.2
 module load scipy-stack
 
-# We need to actually merge all the files together now
-# Generating the files we need to merge, we do this in parallel within one chromosome for all the splits
-
-# for now, we'll use the chromo list in the simulation directory
-#names=($(cat ./chromos14.txt))
+# To run 24 jobs in array, based on this list of chromosomes
 names=($(cat ./chromos.txt))
-#names=($(cat ./chromosontthird.txt))
-
 files=${names[${SLURM_ARRAY_TASK_ID}]}
-echo "${files}"
 
+# The input: 
+# 1: Project name and location
+# 2: The type of SVs to annotate (ie. sv or svSim). This is a subdirectory that holds the vcf/csv file
+# 3: Cell line name to use for epigenomic annotation (must lead to the location of the ENCODE files)
+
+# Creating a directory which will hold these chromosomes files
 mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/merge_FeatMatrix
 mkdir /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/merge_FeatMatrix/${files}
+
+# The following:
+# Loops through all the splits for a particular chromosome
+# For insertions and deletions, we handle merging separately, since epigenetic features differ (ie. we cannot annotate the epigenetic state as per the inserted sequence)
+# We push the merging python script, merge_FeatMatrix.py, which systematically merges the SVs as per their unique ID
+# The processing we do for each file differs sligtly, therefore we "manually" control each file input
 
 cat /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/SVlen/${files}/splitnames.txt | while read line
 	do
 	loc=$(basename "$line")
 	locnoext=$(basename "${loc%.*}")
-	echo "${locnoext}"
 
-#	head /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/seqFeaturesSV/${files}/${locnoext}.withSVSeqFeat.csv
-#	head /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/seqFeatures/${files}/${locnoext}.with2000SeqFeat.csv
-
-	python merge_FeatMatrix_240530.py \
+	python merge_FeatMatrix.py \
 $1 $2 ${locnoext} \
 ${files} DEL \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/seqFeaturesSV/${files}/${locnoext}.withSVSeqFeat.csv \
@@ -68,7 +67,7 @@ ${files} DEL \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/ENCODE/$3/${files}/${locnoext}.EpiFeatSVs.csv \
 >> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/merge_FeatMatrix_240530.py.txt
 
-        python merge_FeatMatrix_240530.py \
+        python merge_FeatMatrix.py \
 $1 $2 ${locnoext} \
 ${files} INS \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/seqFeaturesSV/${files}/${locnoext}.withSVSeqFeat.csv \
@@ -96,8 +95,7 @@ ${files} INS \
 >> /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/merge_FeatMatrix_240530.py.txt
 	done
 
-# we need to merge the files together
-#for ext in DEL deletion INS insertion ;
+# We merge these files all together 
 for ext in DEL INS ;
 	do
 	cat /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/merge_FeatMatrix/${files}/*.${ext}.FeatMatrix.csv \
@@ -105,41 +103,6 @@ for ext in DEL INS ;
 	done
 
 
-
-# NOTE = 30MINS IS NOT ENOUGH FOR 20 SPLITS
-
-# how to generate: variants_freeze4_sv_insdel_alt.vcf.SVlength.Simulations.csv within SVlen: See /home/nboev/projects/def-sushant/nboev/data/SimulatedSVs/HGSVC2_v2_integrated_callset/execute_PostProcessing_240502.sh
-
-# Running:
-# sbatch execute_Master1Merge_240530.sh HGSVC2_v2_integrated_callset sv H1
-# sbatch execute_Master1Merge_240530.sh HGSVC2_v2_integrated_callset svSim H1
-
-# Running for 1000Gs
-# sbatch execute_Master1Merge_240530.sh 20220422_3202_phased_SNV_INDEL_SV_bychrom SVTrue_typedeletion_resTrueIND
-# Running for 1000Gs simulations
-# sbatch execute_Master1Merge_240530.sh 20220422_3202_phased_SNV_INDEL_SV_bychrom SVTrue_typedeletion_resTrueINDsvSIM
-
-# Running for 1000Gs updated file
-# sbatch execute_Master1Merge_240530.sh 20220422_3202_phased_SNV_INDEL_SV_bychrom SVTrue_typedeletion_resTrue H1
-# sbatch execute_Master1Merge_240530.sh 20220422_3202_phased_SNV_INDEL_SV_bychrom SVTrue_typedeletion_resTrueSIM H1
-
-# Running on the CPTAC validation set: AFTER ADDING THE epigenetic part
-# sbatch execute_Master1Merge_240530.sh CPTAC3 bf584e4c-f981-473e-b40a-c73d7f3699d2.wgs.sanger_raw_pindel.raw_somatic_mutation HelaS3
-# sbatch execute_Master1Merge_240530.sh CPTAC3 bf584e4c-f981-473e-b40a-c73d7f3699d2.wgs.sanger_raw_pindel.SIM HelaS3
-# sbatch execute_Master1Merge_240530.sh CPTAC3 fcc8a59b-19b0-4b78-9ca1-f5b9e580f183.wgs.sanger_raw_pindel.raw_somatic_mutation HelaS3
-# sbatch execute_Master1Merge_240530.sh CPTAC3 fcc8a59b-19b0-4b78-9ca1-f5b9e580f183.wgs.sanger_raw_pindel.SIM HelaS3
-
-# Running on Belyeu2021 = 2024/07/25
-# sbatch execute_Master1Merge_240530.sh Belyeu2021 sv H1
-# sbatch execute_Master1Merge_240530.sh Belyeu2021 svSim H1
-
-# Running for HGSVC3 = 2024/08/19
-# sbatch execute_Master1Merge_240530.sh allHGSVC3_20240415_Freeze4_GRCh38 sv H1
-# sbatch execute_Master1Merge_240530.sh allHGSVC3_20240415_Freeze4_GRCh38 svSim H1
-
-# Running for ONT data = 2024/09/10
-# sbatch execute_Master1Merge_240530.sh ONT_1000Gs_100FREEZE sv H1
-# sbatch execute_Master1Merge_240530.sh ONT_1000Gs_100FREEZE svSim H1
-
-# Running for DepMap = 2024/10/20
-# sbatch execute_Master1Merge_240530.sh DepMap CA922 H1
+# example :
+# sbatch execute_Master1Merge.sh HGSVC2_v2_integrated_callset sv H1
+# sbatch execute_Master1Merge.sh HGSVC2_v2_integrated_callset svSim H1
