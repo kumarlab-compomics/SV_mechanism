46808550899c8d92d41b192ff2de50f6dcc46001
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-10 15:13:48 -0500

    Update and rename adding_chromoBand_230514.py to adding_chromoBand.py

--- preprocess/adding_chromoBand.py
+++ preprocess/adding_chromoBand.py
@@ -8,17 +8,17 @@ from pytz import timezone
 
 print('START TIME:', datetime.datetime.now(timezone('EST')))
 
-#loading in the chromosome band data, we don't need this yet... our goal is just to see if we can load this in...
-# loading in the csv (which holds all the chromo)
-print(argv[1])
+# In this script, we will determine the chromoband state of a given breakpoint as per ChromosomeBand_hg38.txt
+# This file was downloaded here: http://genome.ucsc.edu/cgi-bin/hgTables
+
+# Data inputs sent in from the execution files
 svs = pd.read_csv(str(argv[1]), comment='#', sep='\t')
 project = str(argv[2])
 loc = str(argv[3])
 filename = str(argv[4])
 chr = str(argv[5])
-print(svs.head())
 
-# we're only going to do one chromo at a time
+# The file we are using to annotate. Essentially has the start/stop coordinates and the chromoband state/ Giemsa stain.
 chromos = pd.read_csv('/home/nboev/projects/def-sushant/nboev/data/chromoBand/ChromosomeBand_hg38.txt', delimiter = "\t")
 chromos = chromos[chromos['#chrom'] == chr]
 
@@ -28,23 +28,18 @@ print('og rows:', ogrow)
 df_svs = svs[svs['CHROM'] == chr]
 df_chromos = chromos[chromos['#chrom'] == chr]
 
+# We use the IntervalIndex function to create intervals in the index. This is then compared with the SV position to produce the gieStain column in the vcf/csv file
 idx = pd.IntervalIndex.from_arrays( df_chromos['chromStart'], df_chromos['chromEnd'], closed='left')
 df_chromos.index=idx
-
 df_svs['gieStain']=df_chromos.loc[df_svs.POS,'gieStain'].values
+
 newrow = len(df_svs)
 print('new rows:', newrow)
 
-# checking if the rows match (they should)
-# we need to assure we don't have over writing
+# Checking if we lose any of the rows in this process. If the new and old number of rows match, we save the annotated file
 if ogrow == newrow:
-	print('good merge')
 	df_svs.to_csv('/home/nboev/projects/def-sushant/nboev/preprocess/'+project+'/'+loc+'/chromoBand/'+chr+'/' +filename+ '.SVlength.csv', sep='\t', index=False)
 else:
 	print('poor merged')
 
-
 print('END TIME:', datetime.datetime.now(timezone('EST')))
-
-
-
