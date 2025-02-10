ebdc99679f8e0ef9b4b80c7751f2ab9ce0dc9bba
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-10 10:20:30 -0500

    Update and rename generating_vcfDEL_240430.py to generating_vcfDEL.py

--- simulations/generating_vcfDEL.py
+++ simulations/generating_vcfDEL.py
@@ -11,19 +11,18 @@ from Bio import SeqIO
 print('\n ********************************')
 print('START TIME:', datetime.datetime.now(timezone('EST')))
 
-# this is for the processing of deletions only
+# In this script, we will take the simulated bed file from SURVIVOR to generate a vcf-like file, whereby we will need to refrieve the REF sequence, which we will use to annotate and compare to the real SV
+# This script is only used when requesting simulations for deletions
+
+# Data inputs sent in from the execution files
 bedfile = str(argv[1])
 parent = os.path.dirname(bedfile)
-print(parent)
-
 chromo = str(argv[2])
 naming = str(argv[3])
 naming = naming.replace('.param.txt','')
-
-# how many rows should we take at the end = we want a uniform number of simulations per real variant
 maxnum = str(argv[4])
 
-# cleaning up the bed file
+# We need to clean up the bed file provided and add some columns to make it obvious it was a simulation
 bed = pd.read_csv(bedfile, header= None, index_col=None, sep='\t')
 bed = bed.rename(columns={0: 'CHROM', 1: 'POS', 4:'SV_Type' })
 bed = bed[bed.SV_Type=='DEL']
@@ -32,15 +31,14 @@ bed['SV_logic'] = True
 bed['Sim'] = True
 bed['Sim_ID'] = naming
 
-print(bed.head())
-
+# Grabbing the simulated inserted sequence from the fasta
 for idx, row in bed.iterrows():
 	bed.loc[idx, 'ID'] = row['CHROM']+'-'+str(row['POS'])+'-'+row['SV_Type']+'-'+str(row['SVlen'])
 	bed.loc[idx, 'identifier'] = row['CHROM']+'-'+str(row['POS'])
 
-# this is not going to work! we need to add the dir name etc..
 
-# we need to pull sequences from faidx
+# In this case, we need to pull the REF sequence using the reference genome and the locations from the bed file
+# We interact with the shell and use samtools' faidx to pull sequences. We then add this to the REF column, as per a vcf-like format
 for idx, row in bed.iterrows():
 	os.system('/bin/bash -c "samtools faidx /home/nboev/projects/def-sushant/nboev/data/Genome/hg38.fa "' +row.CHROM+'":"' +str(row.POS)+ '"-"' +str(row.POS+row.SVlen)+'" -o "'+parent+'"/"'+str(row.CHROM)+str(row.POS)+str(row.SVlen)+'".fasta"')
 	record = SeqIO.read(parent+"/"+str(row.CHROM)+str(row.POS)+str(row.SVlen)+".fasta", "fasta")
@@ -48,13 +46,9 @@ for idx, row in bed.iterrows():
 	bed.loc[idx, 'ALT'] = str(record.seq).upper()[0]
 	os.system('/bin/bash -c "rm "'+parent+'"/"'+str(row.CHROM)+str(row.POS)+str(row.SVlen)+'".fasta"')
 
-print(bed.head())
-
-# we only want the the max number of entries
+# We are only requesting the maxnum (I chose 100), simulations for each real SV
 bed = bed.head(int(maxnum))
-print(bed)
-
 bed = bed[['CHROM', 'POS', 'ID', 'REF', 'ALT', 'SV_Type', 'SVlen', 'SV_logic', 'Sim', 'Sim_ID']]
+
+# Saving this set of simulations
 bed.to_csv(parent+'/'+naming+'.csv', sep='\t', index=False)
-
-
