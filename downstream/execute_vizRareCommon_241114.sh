3772f5e84dc2d910f4a24eb75644b72fcd72c87e
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-28 14:11:24 -0500

    Update execute_vizRareCommon_241114.sh

--- downstream/execute_vizRareCommon_241114.sh
+++ downstream/execute_vizRareCommon_241114.sh
@@ -9,61 +9,38 @@
 #SBATCH --error=viz.e
 #SBATCH --qos=privilege
 
+# Loading in virtual environment and modules
 module --force purge ; module load StdEnv/2020
 source $HOME/virtenv/bin/activate
 module load bedtools
 
-# This script pulls out the common, rares and denovos amonng the 602 trios but removes all duplicates
-# We also care about inheritence patterns in this case (ie. not all 3000 inds are represented)
-#python analysis_vizRareCommon_241114.py \
-#>> ./SVTrue_typedeletion_resTrue/analysis_vizRareCommon_241114.py.txt
+# The input: 
+# 1: The location of the original SV .csv (holds SVlength AND genotype information)
+# 2: The location of the igsr_samples.tsv, which holds the ancestral information for each individual
 
-
-# In this case, we've used all the 3000 inds and pulled out the rare vars, which we will use to calculate % Fuzzys per subpops
-#mkdir /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd
-#mkdir /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/nonAFR_high
-#mkdir /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/nonAFR_low
-#mkdir /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/AFR_high
-#mkdir /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/AFR_low
+# Making a directory to hold outputs
 mkdir /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/popRates
 
-#python analysis_vizRaresAll_241118.py \
-#>> ./SVTrue_typedeletion_resTrue/analysis_vizRaresAll_241118.py.txt
+# will need to delete these two!!!
+#$1 = '/home/nboev/projects/def-sushant/nboev/preprocess/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/SVlen/SVTrue_typedeletion_resTrue.csv'
+#$2 = /home/nboev/projects/def-sushant/nboev/data/phased_SNV_INDEL_SV_20220422_3202/igsr_samples.tsv'
 
-# Usisng all the rare ones, we're now going to see which genes have been hit
-#for k in fuzzy0 fuzzy1 ;
-#	do
-#	for j in AFR_high AFR_low nonAFR_high nonAFR_low ;
-#		do
-#		for filename in /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/${j}/*bed
-#			do
-#			file=$(basename "$filename")
-#			hg_string=$(echo "$file" | awk -F. '{print $2}')
-#
-#			bedtools intersect -wa \
-#-a /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/${j}/${j}.${hg_string}.HOMOpreds.${k}.bed \
-#-b /home/nboev/projects/def-sushant/nboev/data/RefSeq/hg38.ncbiRefSeq_onlyCDS.bed | \
-#awk '!seen[$0]++' \
-#> /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/${j}/${j}.${hg_string}.HOMOpreds.${k}clean.bed
+# Pushing the python script, generating_bedRaresInds.py. This script generates individual-level SVs for both clusters (clusterI and clusterII), among rare SVs. 
+python generating_bedRaresInds.py \
+$1 \
+$2 \
+>> ./SVTrue_typedeletion_resTrue/analysis_vizRaresAll_241118.py.txt
 
-#			bedtools intersect -wa \
-#-a /home/nboev/projects/def-sushant/nboev/data/RefSeq/hg38.ncbiRefSeq_onlyCDS.bed \
-#-b /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/${j}/${j}.${hg_string}.HOMOpreds.${k}clean.bed | \
-#awk '{ print $NF }' | sort | uniq \
-#> /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/${j}/${j}.${hg_string}.HOMOpreds.${k}cleanGENES.bed
-
-#			rm /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/${j}/${j}.${hg_string}.HOMOpreds.${k}clean.bed
-#			done
-#		done
-#	done
-
-# we need to figure out the ind level recombination rates:
+# We loop through all the populuations in 1KG (note: We exluded the individual who is a known admixture)
+# For each population, we concatenate the population rates. 
+# Then, for the two bed files (ClusterI and clusterII), we find the individual-level bed files.
+# Finally, we use bedtools to intersect the appropriate recombination rate bed file to the SV bed file. 
 for i in ACB ASW BEB CDX CEU CHB CHS CLM ESN FIN GBR GIH GWD IBS ITU JPT KHV LWK MSL MXL PEL PJL PUR STU TSI YRI;
 	do
 	cat /home/nboev/projects/def-sushant/nboev/data/popRecombRate/hg38/${i}/${i}_recombination_map_hg38_chr_{1..22}.bed |uniq \
 > /home/nboev/projects/def-sushant/nboev/data/popRecombRate/hg38/${i}/${i}_recombination_map_hg38.bed
 
-	for k in fuzzy0 fuzzy1 ;
+	for k in clusterI clusterII ;
 		do
 		for filename in /home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/${i}.*.HOMOpreds.${k}.bed ;
 			do
@@ -78,6 +55,6 @@ for i in ACB ASW BEB CDX CEU CHB CHS CLM ESN FIN GBR GIH GWD IBS ITU JPT KHV LWK
 		done
 	done
 
-
-python analysis_RarepopRecomb_241127.py \
+# ** work on this part!!
+python analysis_RarepopRecomb.py \
 >> ./SVTrue_typedeletion_resTrue/analysis_RarepopRecomb_241127.py.txt
