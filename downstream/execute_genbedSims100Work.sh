1a6200b9a631039cd72977a08edbb32b46af589c
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-28 09:46:26 -0500

    Update and rename execute_genbedSims100Work_241106.sh to execute_genbedSims100Work.sh

--- downstream/execute_genbedSims100Work.sh
+++ downstream/execute_genbedSims100Work.sh
@@ -9,21 +9,28 @@
 #SBATCH --error=bedWork.e
 #SBATCH --qos=privilege
 
+# Loading in virtual environment and modules
 source $HOME/virtenv/bin/activate
 module load samtools
 module load bedtools
 
-# Comparing predictions (testing) to other genomic features
+# The input: 
+# 1: Project name/ location
+# 2: sv
+# Note: We need to point to csvs which hold the homology based results along with the z-score merged files. We are generating bed files to represent these strata by SV type
 
+# Creating a directory to store the bed files
 mkdir /home/nboev/projects/def-sushant/nboev/analysis/$1/$2/IDmechsvSIM/20240625/splitsChromo/bedWork100s
 
+# We loop through SV types, homology-based label, and each chromosome to push the generating_bedSim100sWork.py script. 
+# We then concatenate these together and save the larger file
 for k in insertion deletion ;
 	do
-	for j in HR SSAaEJ NHEJ ;
+	for j in HLH ILH NLH ;
 		do
 		for i in X Y 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 ;
 			do
-			python generating_bedSim100sWork_241106.py \
+			python generating_bedSim100sWork.py \
 /home/nboev/projects/def-sushant/nboev/analysis/$1/$2/IDmechsvSIM/20240625/splitsChromo/models/results/chr${i}.${k}.HOMOpreds.tsv \
 /home/nboev/projects/def-sushant/nboev/preprocess/$1/$2/zscore/chr${i}.20240625.chr${i}.${k}_FeatMatrixzscore.csv \
 $1 $2 \
@@ -45,8 +52,5 @@ ${j} \
 
 #How to run:
 # sbatch execute_genbedSims100Work_241106.sh HGSVC2_v2_integrated_callset sv
-# sbatch execute_genbedSims100Work_241106.sh 20220422_3202_phased_SNV_INDEL_SV_bychrom SVTrue_typedeletion_resTrue
-# sbatch execute_genbedSims100Work_241106.sh ONT_1000Gs_100FREEZE sv
-
 
 
