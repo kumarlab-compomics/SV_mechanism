9c3a39856dd1b378029eeca172c641f1b6b6ec97
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-02-28 14:36:18 -0500

    Update and rename analysis_RarepopRecomb_241127.py to analysis_RarepopRecomb.py

--- downstream/analysis_RarepopRecomb.py
+++ downstream/analysis_RarepopRecomb.py
@@ -11,19 +11,23 @@ import os
 print('\n **********************')
 print('START TIME:', datetime.datetime.now(timezone('EST')))
 
+# In this script, we agglomerate all the recombination-rate annotated bed files (for each individual, and for both cluster I and II), to calculate a mean recombination rate. 
+
+# Grabbing all the bed files in the popRates directory, whereby these are already annotated with recombination rates
+# We loop through all these files and split the filename to identify the population code, and cluster, then concatenate across
 path='/home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/popRates/'
 all_files = glob.glob(os.path.join(path , "*.bed"))
-print(all_files)
 li1 = []
 
 for filename in all_files:
 	df = pd.read_csv(filename,  sep = '\t', header=None)
-	df['Superpopulation code'] = filename.split('popRates/')[1].split('.')[0]
+	df['Population code'] = filename.split('popRates/')[1].split('.')[0]
 	df['ind'] = filename.split('popRates/')[1].split('.')[1]
-	df['fuzzy'] = filename.split('popRates/')[1].split('.')[3]
+	df['ClusterBasedLabel'] = filename.split('popRates/')[1].split('.')[3]
 
 	li1.append(df)
 lol = pd.concat(li1, axis=0, ignore_index=True)
-means = lol.groupby(['ind', 'Superpopulation code', 'fuzzy'])[6].mean().reset_index()
 
+# We use a groupby the individual, population and cluster to calculate the mean recombination rate. This summarized dataset is saved as meanRareRecombRates.tsv
+means = lol.groupby(['ind', 'Population code', 'ClusterBasedLabel'])[6].mean().reset_index()
 means.to_csv("/home/nboev/projects/def-sushant/nboev/analysis/20220422_3202_phased_SNV_INDEL_SV_bychrom/SVTrue_typedeletion_resTrue/IDmechsvSIM/20240625/splitsChromo/bedRareInd/popRates/meanRareRecombRates.tsv" , sep='\t', header=True, index=False, )
