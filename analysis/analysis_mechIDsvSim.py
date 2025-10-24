722d0ffe9f3f4d6cedd8a487c06d620850517637
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-10-24 16:39:03 -0400

    Update and rename analysis_mechIDsvSim_250616.py to analysis_mechIDsvSim.py

--- analysis/analysis_mechIDsvSim.py
+++ analysis/analysis_mechIDsvSim.py
@@ -9,32 +9,28 @@ from scipy.stats import percentileofscore
 print('\n **********************')
 print('START TIME:', datetime.datetime.now(timezone('EST')))
 
-# Getting the merged feature matrix
-print(argv[1])
+# In this script, we take in a vcf/csv batch file that we have annotated and label the real SVs with their local homology label
+# Recall, the file we are passing in contains both real and simulated SVs. Therefore, some filtering will be required.
+
+# Data inputs sent in from the execution files
 chromo = str(argv[2])
 type = str(argv[3])
 project = str(argv[4])
 loc = str(argv[5])
-
-# this holds the identities of each sv
 result = pd.read_csv(str(argv[1]), comment='#', sep='\t')
-print(result.columns)
-print(result.head())
-print(len(result))
 
+# Here is the filtering. We require the Sim binary column to be False. We also require SURVIVOR was successfully able to generate 100 simulations for this real SV
 result = result[result['Sim'] == False]
 result = result[result.n_zscore==101]
-print(result.head())
-print(len(result))
-
 ids = list(result.ID)
-
 result[['chr','POS', 'type', 'len']] = result['ID'].str.split('-',expand=True)
 result["len"] = pd.to_numeric(result["len"])
 result = result.drop(['ID','chr', 'POS', 'type'] , axis=1)
 
-print(result.columns)
 forced = result
+
+# We use the iter1_pident, iter1_length and iter1_prebrkpt_loose columns from the Blast annotations (processing to calculate the coverage)
+# Based on the thresholds, we label SVs with either a : HLH, ILH, NLH, or Undefined label. 
 
 for idx, row in forced.iterrows():
 	if (row.iter1_length>100) & (row.iter1_pident>90)&(row['iter1_prebrkpt_loose']=='True')&(row['iter1_postbrkpt_loose']=='True'):
@@ -48,8 +44,7 @@ for idx, row in forced.iterrows():
 
 forced['ID'] = ids
 
-print(forced.head())
-
+# Save the final labelled file
 forced.to_csv('/home/nboev/projects/def-sushant/nboev/analysis/'+project+'/'+loc+'/IDmechsvSIM/20250616/'+chromo+'.'+type+'_ID0616.tsv', sep='\t', index=False)
 
 print('end of script')
