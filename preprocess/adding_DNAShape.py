1784d46977f5a78f659a3289bec64a84b5a066ec
16nbb1 <68873262+16nbb1@users.noreply.github.com>
2025-10-24 16:14:10 -0400

    Update and rename adding_DNAShape_250610.py to adding_DNAShape.py
    Update the DNA Shape script (ie. ind of Blast script now)

--- preprocess/adding_DNAShape.py
+++ preprocess/adding_DNAShape.py
@@ -11,17 +11,23 @@ from pytz import timezone
 print('\n**********************')
 print('START TIME:', datetime.datetime.now(timezone('EST')))
 
+# In this script, we use the +-2000bp flanks and SVs to generate DNA Shape profiles using DNA Shape R
 
-print(argv[1])
 svs = pd.read_csv(str(argv[1]), comment='#', sep='\t')
 project = str(argv[2])
 loc = str(argv[3])
 filename = str(argv[4])
 chr = str(argv[5])
-print(svs.head())
 size = str(argv[6])
 
-def pullBlast (df, flank):
+'''
+The function, pullShape, generates fasta files from the flanking and SV sequences, and sends them to the script, adding_SVDNAshapeR.R, to run DNAShapeR. Finally, the function deletes any intermediate files
+The input: 
+	df: The file to annotate
+	flank: the length of the total flank+SV (ie. 2000bp)
+'''
+
+def pullShape (df, flank):
 	prename = "pre_flank_seq_"+flank
 	postname = "post_flank_seq_"+flank
 
@@ -46,8 +52,7 @@ def pullBlast (df, flank):
 		with open('/home/nboev/projects/def-sushant/nboev/preprocess/'+project+'/'+loc+'/Blast/'+chr+'/premerge/'+filename+'SV_testing.fa', 'w+') as fh:
 			fh.write(sv)
 
-# in this version, were trying to run DNA shape at the same time using the fa we already have
-		os.system('/bin/bash -c "Rscript adding_SVDNAshapeR_230630.R /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/Blast/"'+chr+'"/premerge/"'+filename+'"SV_testing.fa /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/Blast/"'+chr+'"/premerge/"'+filename+'"PRE_testing.fa /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/Blast/"'+chr+'"/premerge/"'+filename+'"POST_testing.fa "'+ID+'" > /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+ '"/adding_SVDNAshapeR_230630.R.txt"')
+		os.system('/bin/bash -c "Rscript adding_SVDNAshapeR.R /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/Blast/"'+chr+'"/premerge/"'+filename+'"SV_testing.fa /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/Blast/"'+chr+'"/premerge/"'+filename+'"PRE_testing.fa /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/Blast/"'+chr+'"/premerge/"'+filename+'"POST_testing.fa "'+ID+'" > /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+ '"/adding_SVDNAshapeR_230630.R.txt"')
 		result = pd.read_csv('/home/nboev/projects/def-sushant/nboev/preprocess/'+project+'/'+loc+'/Blast/'+chr+'/premerge/'+filename+'SV_testing.csv', comment='#', sep=',')
 		grabbing.append(result)
 
@@ -59,17 +64,13 @@ def pullBlast (df, flank):
 		os.system('/bin/bash -c "rm /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/Blast/"'+chr+'"/premerge/"'+filename+i+'"_testing.fa.ProT"')
 		os.system('/bin/bash -c "rm /home/nboev/projects/def-sushant/nboev/preprocess/"'+project+'"/"'+loc+'"/Blast/"'+chr+'"/premerge/"'+filename+i+'"_testing.fa.Roll"')
 
-
 	allresults = pd.concat(grabbing, axis=0, ignore_index=True)
-
 	return allresults
 
 
-# calling the Blast function
-lol = pullBlast(svs, size)
-print(lol.head())
-
-lol.to_csv('/home/nboev/projects/def-sushant/nboev/preprocess/'+project+'/'+loc+'/DNAshape/'+chr+'/postmerge/' +filename+ '.DNAshape.csv', sep='\t', index=False)
+# Calling the pullShape function
+df = pullShape(svs, size)
+df.to_csv('/home/nboev/projects/def-sushant/nboev/preprocess/'+project+'/'+loc+'/DNAshape/'+chr+'/postmerge/' +filename+ '.DNAshape.csv', sep='\t', index=False)
 
 print('END TIME:', datetime.datetime.now(timezone('EST')))
 
